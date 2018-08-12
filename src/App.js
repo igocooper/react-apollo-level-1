4f3aa525a87a38bb00316d1a12f15992b50bccfd
Oleg Lytvyn <igocooper@gmail.com>
2018-08-12 14:38:07 +0300

    Lesson 9. Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// react router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Apollo stuff 
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider, Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
+
+
+// our components
+import Post from './Posts/Post';
+import Posts from './Posts/Posts';
 
 const client = new ApolloClient({
   uri: 'https://api-euwest.graphcms.com/v1/cjko4kc4p1hek01dls0gy08i9/master'
 });
 
-const POSTS_QUERY = gql`
-  query allPosts {
-    posts {
-      title
-      id
-      body
-    }
-  }
-`
-
-// client.query({
-//   query: testQuery
-// }).then(res => console.log(res));
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
+        <Router>
         <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Query query={POSTS_QUERY}>
-          { ({data, loading}) => {
-            if (loading) return 'Loading ...'
-            const { posts }  = data;
-            return posts.map( post => <h1 key={post.id}>{post.title}</h1> ) 
-          }}
-        </Query>
-      </div>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Switch>
+            <Route exact path="/" component={Posts}/>
+            <Route path="/post/:id" component={Post}/>
+          </Switch>
+
+        </div>
+        </Router>
       </ApolloProvider>
     );
   }
