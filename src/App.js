3128e4d410bb180f189334716278c2913b06bc99
Oleg Lytvyn <igocooper@gmail.com>
2018-08-12 11:30:20 +0300

    Lesson 4 introduced Query component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 // Apollo stuff 
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const client = new ApolloClient({
   uri: 'https://api-euwest.graphcms.com/v1/cjko4kc4p1hek01dls0gy08i9/master'
 });
 
-const testQuery = gql`
+const POSTS_QUERY = gql`
   {
     posts {
       title
@@ -20,9 +20,9 @@ const testQuery = gql`
   }
 `
 
-client.query({
-  query: testQuery
-}).then(res => console.log(res));
+// client.query({
+//   query: testQuery
+// }).then(res => console.log(res));
 
 class App extends Component {
   render() {
@@ -33,9 +33,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Query query={POSTS_QUERY}>
+          { ({data, loading}) => {
+            if (loading) return 'Loading ...'
+            const { posts }  = data;
+            return posts.map( post => <h1 key={post.id}>{post.title}</h1> ) 
+          }}
+        </Query>
       </div>
       </ApolloProvider>
     );
