da502358cee7d226c75d8858e28b687b4ea8cf7e
Oleg Lytvyn <igocooper@gmail.com>
2018-08-13 12:29:47 +0300

    Lesson 21. Added persisted cache

--- src/App.js
+++ src/App.js
@@ -3,50 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 // react router
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-// Apollo stuff 
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 
 // our components
 import Post from './Posts/Post';
 import NewPost from './Posts/NewPost';
 import Posts from './Posts/Posts';
 
-const defaultState = {
-  isEditMode: false
-}
-
-const client = new ApolloClient({
-  uri: 'https://api-euwest.graphcms.com/v1/cjko4kc4p1hek01dls0gy08i9/master',
-  clientState: {
-    defaults: defaultState,
-    resolvers: {}
-  }
-});
 
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
         <Router>
-        <div className="App">
-          <header className="App-header">
-            <Link to="/">
-              <h1 className="App-title">Welcome to React Apollo 🚀</h1>
-            </Link>
-          </header>
-          <main>
-            <Switch>
-              <Route exact path="/" component={Posts}/>
-              <Route exact path="/post/new" component={NewPost}/>
-              <Route path="/post/:id" component={Post}/>
-            </Switch>
-          </main>
-
-        </div>
+          <div className="App">
+            <header className="App-header">
+              <Link to="/">
+                <h1 className="App-title">Welcome to React Apollo 🚀</h1>
+              </Link>
+            </header>
+            <main>
+              <Switch>
+                <Route exact path="/" component={Posts}/>
+                <Route exact path="/post/new" component={NewPost}/>
+                <Route path="/post/:id" component={Post}/>
+              </Switch>
+            </main>
+          </div>
         </Router>
-      </ApolloProvider>
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Apollo stuff 
+import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import { ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: window.localStorage
+})
+  .then( () => {
+    const defaultState = {
+      isEditMode: false
+    }
+
+    const client = new ApolloClient({
+      cache,
+      uri: 'https://api-euwest.graphcms.com/v1/cjko4kc4p1hek01dls0gy08i9/master',
+      clientState: {
+        defaults: defaultState,
+        resolvers: {}
+      }
+    });
+
+    ReactDOM.render(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>, 
+      document.getElementById('root')
+    );
+    registerServiceWorker();
+
+});
